ecc127f88e672f8d3129a5550b5eb7b7eec09fb7
Jeff Wilkey <jeffrotull92@gmail.com>
2019-07-19 15:50:53 -0700

    pass playlist / track props down from App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
+import { Playlist } from '../Playlist/Playlist';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{ name: 'Are U Dead Yet?', artist: 'Children of Bodom', album: 'Are U Dead Yet?' }],
+      playlistName: "Jeff's mix",
+      playlistTracks: [{ name: 'Alligator', artist: 'Of Monsters and Men', album: 'Fever Dream' }]
+    }
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    if (this.state.playlistTracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    } else {
+      const playlistTracks = new Array(...this.state.playlistTracks);
+      playlistTracks.push(track);
+      this.setState({ playlistTracks });
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          {/* Add SearchBar Component */}
+          <SearchBar/>
           <div className="App-playlist">
-            { /* Add SearchResults Component */ }
-            { /* Add Playlist Component */ }
+            <SearchResults searchResults={this.state.searchResults}/>
+            <Playlist name={this.state.playlistName} tracks={this.state.playlistTracks}/>
           </div>
         </div>
       </div>
--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Track } from '../Track/Track';
 import './TrackList.css';
 
 export class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        {/* .map() method that will render a set of Track Components */}
+        {this.props.tracks.map(track => <Track key={track.id} track={track}/> )}
       </div>
     )
   }
